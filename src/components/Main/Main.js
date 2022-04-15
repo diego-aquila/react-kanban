23a5f690479fa8d57ab103ca6a793e46ad3bbfa0
PedroPiuma <lpap1000@gmail.com>
2022-04-14 23:49:14 -0300

    fix coluna com cards

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,6 @@
 import './Main.css'
 import Coluna from '../Coluna/Coluna'
 import Cards from '../Cards/Cards'
-// import img from './img/icon-caderneta.png'
 
 const Main = () => {
     return (
@@ -9,23 +8,22 @@ const Main = () => {
             <h1 className='main-title'>Kanban</h1>
             <div className='main-container'>
                 <Coluna nameColumn='To Do' color='blue' img='cardeneta' >
-                    <Cards img='male'>Christmas Banners</Cards >
-                    <Cards img='male'>Redo Portfolio</Cards>
+                    <Cards img='male' label='Label' labelColor='blue'>Christmas Banners</Cards >
+                    <Cards img='male' label='Label' labelColor='blue'>Redo Portfolio</Cards>
                 </Coluna>
                 <Coluna nameColumn='Inprogress' color='orange' img='computer' >
-                    <Cards img='female'>Coffee Break</Cards >
-                    <Cards img='female'>Updating Portfolio</Cards>
+                    <Cards img='female' label='Always' labelColor='orange'>Coffee Break</Cards >
+                    <Cards img='female' label='Webflow' labelColor='orange'>Updating Portfolio</Cards>
                 </Coluna>
                 <Coluna nameColumn='Review' color='purple' img='pencil' >
-                    <Cards img='male'>Release to Figma Community</Cards >
-                    <Cards img='male'>User Feedback</Cards>
-                    <Cards img='male'>User Feedback</Cards>
-                    <Cards img='male'>Background images from humaaans.com</Cards>
+                    <Cards img='male' label='Release' labelColor='purple'>Release to Figma Community</Cards >
+                    <Cards img='male' label='Feedback' labelColor='purple'>User Feedback</Cards>
+                    <Cards img='male' label='Sourcing' labelColor='purple' >Background images from <a href='https://www.humaaans.com/'>humaaans.com</a></Cards>
                 </Coluna>
                 <Coluna nameColumn='Done' color='green' img='rocket' >
-                    <Cards img='female'>Style Guide</Cards >
-                    <Cards img='female'>Component Library</Cards>
-                    <Cards img='female'>Sticker Components</Cards>
+                    <Cards img='female' label='UI' labelColor='green'>Style Guide</Cards >
+                    <Cards img='female' label='UI' labelColor='green'>Component Library</Cards>
+                    <Cards img='female' label='UI' labelColor='green'>Sticker Components</Cards>
                 </Coluna>
             </div>
         </main >
