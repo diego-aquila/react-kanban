39705321a5c46685318def8491eda4df2027d5aa
PedroPiuma <lpap1000@gmail.com>
2022-04-18 22:11:43 -0300

    reorganização dos cards para dentro de coluna.js

--- src/components/Coluna/Coluna.js
+++ src/components/Coluna/Coluna.js
@@ -1,7 +1,29 @@
 import './Coluna.css'
+import Cards from '../Cards/Cards'
+
+const data = {
+    todo: [
+        <Cards img='male' label='Label' labelColor='blue'>Christmas Banners</Cards >,
+        <Cards img='male' label='Label' labelColor='blue'>Redo Portfolio</Cards>
+    ],
+    inprogress: [
+        <Cards img='female' label='Always' labelColor='orange'>Coffee Break</Cards >,
+        <Cards img='female' label='Webflow' labelColor='orange'>Updating Portfolio</Cards>
+    ],
+    review: [
+        <Cards img='male' label='Release' labelColor='purple'>Release to Figma Community</Cards >,
+        <Cards img='male' label='Feedback' labelColor='purple'>User Feedback</Cards>,
+        <Cards img='male' label='Sourcing' labelColor='purple' >Background images from <a href='https://www.humaaans.com/'>humaaans.com</a></Cards>
+    ],
+    done: [
+        <Cards img='female' label='UI' labelColor='green'>Style Guide</Cards >,
+        <Cards img='female' label='UI' labelColor='green'>Component Library</Cards>,
+        <Cards img='female' label='UI' labelColor='green'>Sticker Components</Cards>
+    ]
+}
 
 const Coluna = (props) => {
-    const { nameColumn = 'To Do', color = 'blue', img, children } = props
+    const { nameColumn = 'To Do', color = 'blue', img } = props
     return (
         <div className={`coluna ${color}`}>
             <div className={`border-top border-top__${color}`}></div>
@@ -10,7 +32,7 @@ const Coluna = (props) => {
                 <h2>{nameColumn}</h2>
             </div>
             <div className='column-cards'>
-                {children.map(e => e)}
+                {data[`${nameColumn.replaceAll(' ', '').toLowerCase()}`]}
             </div>
         </div>
     )
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,30 +1,15 @@
 import './Main.css'
 import Coluna from '../Coluna/Coluna'
-import Cards from '../Cards/Cards'
 
 const Main = () => {
     return (
         <main className='main'>
             <h1 className='main-title'>Kanban</h1>
             <div className='main-container'>
-                <Coluna nameColumn='To Do' color='blue' img='cardeneta' >
-                    <Cards img='male' label='Label' labelColor='blue'>Christmas Banners</Cards >
-                    <Cards img='male' label='Label' labelColor='blue'>Redo Portfolio</Cards>
-                </Coluna>
-                <Coluna nameColumn='Inprogress' color='orange' img='computer' >
-                    <Cards img='female' label='Always' labelColor='orange'>Coffee Break</Cards >
-                    <Cards img='female' label='Webflow' labelColor='orange'>Updating Portfolio</Cards>
-                </Coluna>
-                <Coluna nameColumn='Review' color='purple' img='pencil' >
-                    <Cards img='male' label='Release' labelColor='purple'>Release to Figma Community</Cards >
-                    <Cards img='male' label='Feedback' labelColor='purple'>User Feedback</Cards>
-                    <Cards img='male' label='Sourcing' labelColor='purple' >Background images from <a href='https://www.humaaans.com/'>humaaans.com</a></Cards>
-                </Coluna>
-                <Coluna nameColumn='Done' color='green' img='rocket' >
-                    <Cards img='female' label='UI' labelColor='green'>Style Guide</Cards >
-                    <Cards img='female' label='UI' labelColor='green'>Component Library</Cards>
-                    <Cards img='female' label='UI' labelColor='green'>Sticker Components</Cards>
-                </Coluna>
+                <Coluna nameColumn='To Do' color='blue' img='cardeneta' />
+                <Coluna nameColumn='Inprogress' color='orange' img='computer' />
+                <Coluna nameColumn='Review' color='purple' img='pencil' />
+                <Coluna nameColumn='Done' color='green' img='rocket' />
             </div>
         </main >
     )
